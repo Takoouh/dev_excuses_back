78254066cf3c41a1f6fabbdf225834842f568a1e
Florentin <florentin.coyard@gmail.com>
2022-06-14 09:03:09 +0200

    feat(major): added cors library to handle cors ERROR

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 const indexRouter = require("./routes/index");
+
+// allow cors on all routes
+app.use(cors());
 
 /* Allow us to parse body in post requests */
 app.use(express.json());
